bd0ee1c46756bd3ebb3eaa6fccab12f81e5fac37
Fernando Cordisco <fernandorcvilela@hotmail.com>
2022-12-22 19:24:01 -0300

    Adição main e carta

--- jogo/Cartas.java
+++ jogo/Cartas.java
@@ -1,7 +1,51 @@
 package jogo;
 
 public class Cartas {
-    private String nome;
-    private int ataque;
-    private int chanceAcerto;
+    private int quantidadeDeCartas = 16;
+    private String[] nome = new String[quantidadeDeCartas];
+    private int[] ataque = new int[quantidadeDeCartas];
+    private int[] chanceAcerto = new int[quantidadeDeCartas];
+
+    public Cartas(String[] nome, int[] ataque, int[] chanceAcerto) {
+        this.nome = nome;
+        this.ataque = ataque;
+        this.chanceAcerto = chanceAcerto;
+    }
+    public Cartas() {
+        this.nome = nome;
+        this.ataque = ataque;
+        this.chanceAcerto = chanceAcerto;
+    }
+
+    public String[] getNome() {
+        return nome;
+    }
+
+    public void setNome(String[] nome) {
+        this.nome = nome;
+    }
+
+    public int[] getAtaque() {
+        return ataque;
+    }
+
+    public void setAtaque(int[] ataque) {
+        this.ataque = ataque;
+    }
+
+    public int[] getChanceAcerto() {
+        return chanceAcerto;
+    }
+
+    public void setChanceAcerto(int[] chanceAcerto) {
+        this.chanceAcerto = chanceAcerto;
+    }
+
+    Cartas ravena = new Cartas(new String[]{"Ravena"},
+             new int[]{10}, new int[]{10});
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
